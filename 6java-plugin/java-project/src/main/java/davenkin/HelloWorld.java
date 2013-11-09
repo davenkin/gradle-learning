d03d8f5b7ca0ed2fb9ee76c3bea751915f2702c6
davenkin <davenkin@gmail.com>
2013-11-09 12:37:39 +0800

    java plugin

---
+++ 6java-plugin/java-project/src/main/java/davenkin/HelloWorld.java
@@ -0,0 +1,20 @@
+package davenkin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: davenkin
+ * Date: 3/12/13
+ * Time: 8:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+          private final static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+    public String sayHello(){
+        logger.info("Saying Hello World.");
+        return "Hello World.";
+    }
+}
